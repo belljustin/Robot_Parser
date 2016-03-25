e2af57b20770a137686fefffa4e104fea8af2c79
Justin Bell <justin.bell@mail.mcgill.ca>
2016-03-25 19:20:51 -0400

    Passes upper test

--- src/syntax.c
+++ src/syntax.c
@@ -1,7 +1,12 @@
 char *commands[8] = {"TAKEASTEP", "LEFT", "RIGHT", "PICKUP", "DROP", "DETECTMARKER", "TURNON", "TURNOFF"};
 
 void upper(char *string) {
-    
+    int size = sizeof(string); 
+    for (int i=0; i<=size; i++) {
+        if (string[i] > 96 && string[i] < 123) {
+            string[i] -= 32;
+        }
+    } 
 }
 
 int isValidCommand(char *token) {
