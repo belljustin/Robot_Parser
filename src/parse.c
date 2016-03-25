ba1f3c177dc0e337581da21082e32fe9bc0ab190
Justin Bell <justin.bell@mail.mcgill.ca>
2016-03-25 18:31:20 -0400

    Adds nextToken function

--- src/parse.c
+++ src/parse.c
@@ -1,11 +1,22 @@
 #include <string.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 300
+#define TOKEN_SIZE 80 
 
 char buffer[BUFFER_SIZE];
-char original_buffer[BUFFER_SIZE];
+char *bufferPtr = &buffer[0];
+char token[TOKEN_SIZE];
 
 void initBuffer(char *inputLine) {
     strcpy(buffer, inputLine);
-    strcpy(original_buffer, inputLine);     
+}
+
+char* nextToken() {
+    char *startBufferPtr = bufferPtr;
+    while(*bufferPtr != ' ' && *bufferPtr != '\r') {
+        bufferPtr += sizeof(char);
+    }
+    strncpy(token, startBufferPtr, bufferPtr-startBufferPtr); 
+    return token;
 }
