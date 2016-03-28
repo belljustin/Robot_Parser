1a5f510c056851e4d811d47cad6bcab3405cafd7
Justin Bell <justin.bell@mail.mcgill.ca>
2016-03-28 19:28:36 -0400

    adds function that checks that its a comma seperated list of commands

--- src/syntax.c
+++ src/syntax.c
@@ -7,7 +7,7 @@ char *commands[8] = {"TAKEASTEP", "LEFT", "RIGHT", "PICKUP", "DROP",
                     "DETECTMARKER", "TURNON", "TURNOFF"};
 
 void upper(char *string) {
-    int size = sizeof(string); 
+    int size = strlen(string); 
     for (int i=0; i<=size; i++) {
         if (string[i] > 96 && string[i] < 123) {
             string[i] -= 32;
@@ -21,6 +21,28 @@ int isValidCommand(char *token) {
         if (strcmp(token, commands[i]) == 0) return 1;
     } 
     return 0;
+}
+
+int isCommaSeperatedCommands() {
+    while (hasNextToken()) {
+        char token[80];
+        strcpy(token, nextToken());
+        upper(token);
+
+        if (token[strlen(token)-1] == ',') {
+            char command_token[80];
+            strncpy(command_token, token, strlen(token)-1);
+            command_token[strlen(token)-1] = '\0';
+            if (!isValidCommand(command_token)) return 0;
+        } else { 
+            if (!isValidCommand(token)) return 0;
+            char *end_token = nextToken();
+            upper(end_token);
+            if (strcmp(end_token, "END") != 0) return 0; 
+            break;
+        }
+    }
+    return 1;
 }
 
 int isValidExpression(char *expression) {
